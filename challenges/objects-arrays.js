9520b7fe91cc3fdd5820c8d749f871c1f1a81813
Junior <junior.arodriguez@icloud.com>
2019-04-07 20:12:12 -0500

    console.loged for the first for console.logs

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -42,13 +42,13 @@ const dino3 = {
 console.log(dino1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dino3.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dino2.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dino1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.

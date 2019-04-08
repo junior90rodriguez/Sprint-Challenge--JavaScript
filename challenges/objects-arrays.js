f89c6bab8addfa644fb899baec224581ea7fe330
Junior <junior.arodriguez@icloud.com>
2019-04-07 20:27:29 -0500

    compleated request 2 for OBJECTS-ARRAYS.JS

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -93,6 +93,10 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for (i = 0; i < graduates.length; i++) {
+    contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
